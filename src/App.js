bc50a90f4a059a450d0bfbc1987467432f6de3a3
PabloCurty <pablocurty@id.uff.br>
2022-12-01 17:38:13 -0300

    css inline, vazamento css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import MyComponent from "./Components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/*CSS global*/}
       <h1>React com css</h1>
+      {/*CSS de componente*/}
+      <MyComponent />
+      <p>Esté paragrafo é do APP.js</p>
+      {/*Inline CSS*/}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2 px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
     </div>
   );
 }
---
+++ src/Components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1> CSS de component</h1>
+        <p>Este paragrafo é do componente</p>
+        <p className="my-comp-paragraph">Este tb é um paragrafo do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
