678cca1544097f2f20283adfaed2b4ab50f47488
PabloCurty <pablocurty@id.uff.br>
2022-12-03 16:26:22 -0300

    classes dinamicas, css modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import MyComponent from "./Components/MyComponent";
+import { useState } from "react"
+import Title from "./Components/Title";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Pablo");
+  const redTitle = true;
   return (
     <div className="App">
       {/*CSS global*/}
@@ -10,9 +15,32 @@ function App() {
       <MyComponent />
       <p>Esté paragrafo é do APP.js</p>
       {/*Inline CSS*/}
-      <p style={{ color: "blue", padding: "25px", borderTop: "2 px solid red" }}>
+      <p
+        style={{ color: "blue", padding: "25px", borderTop: "2 px solid red" }}
+      >
         Este elemento foi estilizado de forma inline
       </p>
+      {/*CSS Dinâmico*/}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Pablo" ? { color: "green", backgroundColor: "#000" } : null
+        }
+      >
+        CSS dinâmico teste nome useState
+      </h2>
+      {/*Classe Dinâmica*/}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Esse título vai ter classe dinâmica
+      </h2>
+      {/*CSS Modules*/}
+      <Title/>
+
     </div>
   );
 }
---
+++ src/Components/Title.js
@@ -0,0 +1,7 @@
+import styles from "./Title.module.css";
+
+const Title = () => {
+  return <div className={styles["my-title"]}>My Title</div>;
+};
+
+export default Title;
